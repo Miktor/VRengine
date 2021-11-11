1680ac535a2df945c2050f6b75926ef27ee95709
Gladky Dmitry <miktor.ru@gmail.com>
2021-11-11 18:59:32 +0300

    get reflections via spirv-cross

--- src/rendering/shader.cpp
+++ src/rendering/shader.cpp
@@ -1,11 +1,12 @@
 #include "shader.hpp"
 
-#include <shaderc/shaderc.hpp>
 #include <stdexcept>
+
+#include <shaderc/shaderc.hpp>
+#include <spirv-cross/spirv_cross.hpp>
 
 #include "common.hpp"
 #include "platform/platform.hpp"
-#include "shaderc/shaderc.h"
 
 namespace vre::rendering {
 
@@ -20,12 +21,11 @@ shaderc_shader_kind ToShadercType(Shader::Type type) {
   }
 }
 
-shaderc::SpvCompilationResult Compile(const std::string &path, Shader::Type type) {
-  const auto data = platform::Platform::ReadFile(path);
+shaderc::SpvCompilationResult Compile(const std::string &data, const std::string &path, Shader::Type type) {
   shaderc::Compiler compiler;
   shaderc::CompileOptions options;
 
-  //options.SetOptimizationLevel(shaderc_optimization_level_size);
+  // options.SetOptimizationLevel(shaderc_optimization_level_size);
 
   auto result = compiler.CompileGlslToSpv(data.c_str(), ToShadercType(type), path.data(), options);
 
@@ -36,14 +36,29 @@ shaderc::SpvCompilationResult Compile(const std::string &path, Shader::Type type
   return result;
 }
 
+void GetReflectionData(const std::vector<uint32_t> &data) {
+  spirv_cross::Compiler compiler(data);
+
+  auto resources = compiler.get_shader_resources();
+
+  for (auto &resource : resources.uniform_buffers) {
+    unsigned set = compiler.get_decoration(resource.id, spv::DecorationDescriptorSet);
+    unsigned binding = compiler.get_decoration(resource.id, spv::DecorationBinding);
+    SPDLOG_INFO("Image {} at set = {}, binding = {}", resource.name.c_str(), set, binding);
+  }
+}
+
 }  // namespace
 
 Shader::Shader(VkDevice device, Type type, const std::string &path) : device_(device), type_(type), path_(path) {}
 
 VkShaderModule Shader::Init() {
-  auto compiled = Compile(path_, type_);
+  const auto data = platform::Platform::ReadFile(path_);
+  auto compiled = Compile(data, path_, type_);
 
   std::vector<uint32_t> spirv(compiled.cbegin(), compiled.cend());
+  GetReflectionData(spirv);
+
   VkShaderModuleCreateInfo create_info{};
   create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
   create_info.codeSize = spirv.size() * sizeof(uint32_t);

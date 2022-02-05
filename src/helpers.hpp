acbfb0bfe367fe619fc6f910277130e2c2d72cbf
Gladky Dmitry <miktor.ru@gmail.com>
2022-02-06 00:10:04 +0300

    fix assert messages

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
 #ifndef NDEBUG
-#define VR_ASSERT(x)                                                                     \
-  do {                                                                                   \
-    if (!bool(x)) {                                                                      \
-      SPDLOG_ERROR("Assertation failed, %s != true @ %s:%d.\n", #x, __FILE__, __LINE__); \
-      abort();                                                                           \
-    }                                                                                    \
+#define VR_ASSERT(x)                            \
+  do {                                          \
+    if (!bool(x)) {                             \
+      SPDLOG_ERROR("Assertion failed: {}", #x); \
+      abort();                                  \
+    }                                           \
   } while (0)
 #else
 #define VR_ASSERT(x) ((void)0)
 #endif
 
-#define VR_CHECK(x)                                                                     \
-  do {                                                                                   \
-    if (!bool(x)) {                                                                      \
-      SPDLOG_ERROR("Check failed, %s != true @ %s:%d.\n", #x, __FILE__, __LINE__); \
-      abort();                                                                           \
-    }                                                                                    \
+#define VR_CHECK(x)                         \
+  do {                                      \
+    if (!bool(x)) {                         \
+      SPDLOG_ERROR("Check failed: {}", #x); \
+      abort();                              \
+    }                                       \
   } while (0)
 
-#define CHECK_VK_SUCCESS(x)                                                              \
-  do {                                                                                   \
-    if (VK_SUCCESS != (x)) {                                                             \
-      SPDLOG_ERROR("Vulkan error: %s != VK_SUCCESS @ %s:%d.\n", #x, __FILE__, __LINE__); \
-      abort();                                                                           \
-    }                                                                                    \
+#define CHECK_VK_SUCCESS(x)                               \
+  do {                                                    \
+    if (VK_SUCCESS != (x)) {                              \
+      SPDLOG_ERROR("Vulkan error: {} != VK_SUCCESS", #x); \
+      abort();                                            \
+    }                                                     \
   } while (0)
